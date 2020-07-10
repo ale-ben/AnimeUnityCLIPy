3af5337a8d4b013de84c99535eb106bb858d59d9
Alessandro Benetton MBP <aleben98@gmail.com>
2020-07-10 12:26:11 +0200

    Updated VENV

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/alessandrobenetton/Documents/Coding/Python/AnimeUnityCLI/venv"
+set -gx VIRTUAL_ENV "/Users/alessandrobenetton/Documents/Coding/Python/AnimeUnityCLIPy/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
