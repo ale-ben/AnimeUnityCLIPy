2d6e0d1cdd7edaec91f0467732b21845633fe201
Alessandro Benetton MBP <aleben98@gmail.com>
2020-07-15 21:52:03 +0200

    Refactored and commented res_obj_manipulartor

--- res_obj_manipulator.py
+++ res_obj_manipulator.py
@@ -1,28 +1,56 @@
-from AnimeUnityEngine import  logging_aux
+from AnimeUnityEngine import logging_aux, common_classes
+import json
 
 
 @logging_aux.logger_wraps()
 def get_formatted_search_results(res_obj):
-    for i in range(len(res_obj)):
-        del res_obj[i]['user_id']
-        for epI in range(len(res_obj[i]['episodes'])):
-            del res_obj[i]['episodes'][epI]['user_id']
-        res_obj[i]['anime_url'] = f"{res_obj[i]['id']}-{res_obj[i]['slug']}"
-    return res_obj
+    # Per comodità se non è un array lo trasformo in array
+    if not isinstance(search_res, type([])):
+        res_obj = [res_obj]
+    anime_arr = []
+    for anime_ob in res_obj:
+        # Creo oggetto anime e assegno i campi
+        anime = common_classes.Anime(anime_ob['id'], anime_ob['title'], anime_ob['type'], anime_ob['episodes_length'])
+        anime.status = anime_ob['status']
+        anime.year = anime_ob['date']
+        anime.slug = anime_ob['slug']
+        anime.title_eng = anime_ob['title_eng']
+        anime.cover_image = anime_ob['imageurl_cover']
+        anime.thumbnail = anime_ob['imageurl']
+        for ep in anime_ob['episodes']:
+            # Creo oggetto episodio e assegno campi
+            episode = common_classes.Episode(ep['id'], ep['number'], ep['created_at'], ep['link'])
+            # Aggiungo episodio alla lista dell'anime
+            anime.episodes.append(episode)
+        # Aggiungo anime alla lista
+        anime_arr.append(anime)
+    return anime_arr
 
 
+# Cerco anime per id
 @logging_aux.logger_wraps()
-def get_selected_anime_obj_by_id(search_res, id=None):
-    for res in search_res:
-        if str(res['id']) == id:
+def get_selected_anime_obj_by_id(anime_arr, a_id=None):
+    for res in anime_arr:
+        if str(res.a_id) == a_id:
             return res
-    return search_res[0]
+    return anime_arr[0]
 
 
-def get_year(entry):
-    return entry['date']
+def get_year(anime):
+    return anime.year
 
 
-def order_search_res(search_res):
-    search_res.sort(key=get_year)
-    return search_res
+def order_search_res(anime_list):
+    anime_list.sort(key=get_year)
+    return anime_list
+
+
+if __name__ == "__main__":
+    with open('./doc/test_dir/search_result_final.json') as f:
+        search_res = json.load(f)
+
+    anime_obj = get_formatted_search_results(search_res)
+    print(anime_obj)
+    print(order_search_res(anime_obj))
+
+    print(get_selected_anime_obj_by_id(anime_obj, 743))
