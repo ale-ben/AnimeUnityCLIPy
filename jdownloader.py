3608d4c94fdc7aece446c4c0216df456556014be
Alessandro Benetton MBP <aleben98@gmail.com>
2020-07-15 22:27:46 +0200

    Refactored and commented jdownlaoder

--- jdownloader.py
+++ jdownloader.py
@@ -17,11 +17,11 @@ def format_title(old_title):
 
 
 @logging_aux.logger_wraps()
-def create_crawl_file(anime_page=None, crawl_path="", jdownload_path="", season=1):
+def create_crawl_file(anime_page, crawl_path="", jdownload_path="", season=1):
     file_content = ""
 
     # Formatting the string
-    formatted_anime_title = format_title(anime_page['slug'])
+    formatted_anime_title = format_title(anime_page.slug)
     crawl_file = Path(crawl_path) / formatted_anime_title
 
     # Creating the AnimeDir
@@ -29,7 +29,7 @@ def create_crawl_file(anime_page=None, crawl_path="", jdownload_path="", season=
 
     for ep in anime_page['episodes']:
         file_content = file_content + "{\n"
-        file_content = f"{file_content}\ttext= {ep['link']}\n"
+        file_content = f"{file_content}\ttext= {ep.link}\n"
         file_content = f"{file_content}\tdownloadFolder= {jdownloader_anime_path}\n"
         file_content = f"{file_content}\tenabled= true\n"
         file_content = f"{file_content}\tautoStart= true\n"
@@ -42,14 +42,14 @@ def create_crawl_file(anime_page=None, crawl_path="", jdownload_path="", season=
 
 
 @logging_aux.logger_wraps()
-def send_to_jdownloader(anime_page_list=None, crawl_path="", jdownload_path=""):
-    formatted_title = format_title(anime_page_list[0]['slug'])
+def send_to_jdownloader(anime_page_list, crawl_path="", jdownload_path=""):
+    formatted_title = format_title(anime_page_list[0].slug)
     jdownload_path = Path(jdownload_path) / formatted_title
 
     season_num = 0
 
     for anime in anime_page_list:
-        if anime['type'] == 'TV':
+        if anime.type == 'TV':
             season_num = season_num + 1
             create_crawl_file(anime, crawl_path, jdownload_path, season_num)
         else:
