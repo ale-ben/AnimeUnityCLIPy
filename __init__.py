b93ad2c18921431f11e34808a80beb79ea1c4baa
Alessandro Benetton MBP <aleben98@gmail.com>
2020-07-16 10:37:54 +0200

    Fixed init syntax and commented (D104) (PEP257)

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
-#__all__ = ["scraper", "logging_aux", "res_obj_manipulator","jdownloader"]
+"""Interazione con AnimeUnity e manipolazione dati
+
+Modules:
+common_classes -- classi usate per creare vari oggetti (Anime, Episodi, ...)
+jdownloader -- funzioni usate per creare e formattare file crawljob (File usato da jdownloader per auto avviare download)
+json_parser -- funzioni usate per manipolare e convertire json
+logging_aux -- funzioni ausiliarie per il logging
+res_obj_manipulator -- funzioni necessarie per convertire dizionari in classe Anime e formattare i risultati
+scraper -- funzioni necessarie per ottenere i json richiesti dal sito
+"""
+__all__ = ["scraper", "logging_aux", "res_obj_manipulator", "jdownloader", "common_classes"]
